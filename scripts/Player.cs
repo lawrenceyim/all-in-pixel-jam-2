98a72073b3b9c9060980c799d530f1fe315059db
lawrenceyim <lawrence.yim.dev@gmail.com>
2026-09-22 15:32:27 -0700

    Added grounded check.

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,18 +1,39 @@
 using System;
+using System.Linq;
 using Godot;
 
 public partial class Player : Node2D {
+    #region PlayerState
+
+    public enum State {
+        Jumping,
+        Falling,
+        Grounded
+    }
+
+    #endregion
+
+    #region Exports
+
     [Export]
     private AnimatedSprite2D _animatedSprite2D;
 
-    private PlayerStats _stats = new() { MoveSpeed = 100, JumpSpeed = 200, Gravity = 300, MaxFallSpeed = 300 };
+    [Export]
+    private ShapeCast2D _groundedShapeCast;
+
+    #endregion
+
+    private PlayerStats _stats = new() { MoveSpeed = 100, JumpSpeed = 200, JumpDuration = .25f, Gravity = 400, MaxFallSpeed = 300 };
     private Vector2 _velocity = Vector2.Zero;
+    private State _state = State.Falling;
+    private float _jumpTimeLeft = 0;
 
     public override void _Ready() {
         KeyBind.Initialize(); // TODO: Refactor and move elsewhere
     }
 
     public override void _Process(double delta) {
+        _DetermineState(delta);
         _Input(delta);
         _Gravity(delta);
         _Move(delta);
@@ -32,10 +53,33 @@ public partial class Player : Node2D {
 
         if (Input.IsActionJustPressed(KeyBind.Jump)) {
             _velocity.Y = -_stats.JumpSpeed;
+            _jumpTimeLeft = _stats.JumpDuration;
+        }
+    }
+
+    private void _DetermineState(double delta) {
+        if (_state == State.Jumping) {
+            _jumpTimeLeft -= (float)delta;
+            if (_jumpTimeLeft <= 0) {
+                _state = State.Falling;
+            }
+        }
+
+        if (_state is State.Falling or State.Grounded) {
+            bool grounded = Enumerable
+                .Range(0, _groundedShapeCast.GetCollisionCount())
+                .Any(i => _groundedShapeCast.GetCollider(i) is StaticBody2D);
+
+            _state = grounded ? State.Grounded : State.Falling;
         }
     }
 
     private void _Gravity(double delta) {
+        if (_state != State.Falling) {
+            _velocity.Y = Math.Min(_velocity.Y, 0);
+            return;
+        }
+
         _velocity.Y += _stats.Gravity * (float)delta;
         _velocity.Y = Math.Min(_velocity.Y, _stats.MaxFallSpeed);
     }
@@ -48,6 +92,7 @@ public partial class Player : Node2D {
 public record struct PlayerStats {
     public float MoveSpeed { get; init; }
     public float JumpSpeed { get; init; }
+    public float JumpDuration { get; init; }
     public float Gravity { get; init; }
     public float MaxFallSpeed { get; init; }
 }
